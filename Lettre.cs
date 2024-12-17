1f3b0379d4b8581eb9d82dfc334dbbea71e0b3d5
Soso <solal.chatelain@gmail.com>
2024-12-17 11:49:29 +0100

    Update Lettre.cs

--- Lettre.cs
+++ Lettre.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,25 +11,21 @@ namespace Projet_Boogle_Solal_JB
         char caractere;
         int points;
         int occurence;
-
         public char Caractere
         {
             get { return caractere; }
             set { caractere = value; }
         }
-
         public int Points
         {
             get { return points; }
             set { points = value; }
         }
-
         public int Occurence
         {
             get { return occurence; }
             set { occurence = value; }
         }
-
         public Lettre(char caractere, int points, int occurence)
         {
             this.caractere = caractere;
@@ -37,23 +33,20 @@ namespace Projet_Boogle_Solal_JB
             this.occurence = occurence;
 
         }
-
-        public List<Lettre> Convert(string mot, List<Lettre> lettres )
+        public List<Lettre> Convert(string mot, List<Lettre> lettres)
         {
-             List<Lettre> motLettres = new List<Lettre>();
+            List<Lettre> motLettres = new List<Lettre>();
             for (int i = 0; i < mot.Length; i++)
             {
                 for (int j = 0; j < lettres.Count; j++)
                 {
                     if (mot[i] == lettres[j].Caractere)
                     {
-                        motLettres[i]= lettres[j];
+                        motLettres[i] = lettres[j];
                     }
                 }
             }
-             return motLettres;
-
+            return motLettres;
         }
-
     }
 }
