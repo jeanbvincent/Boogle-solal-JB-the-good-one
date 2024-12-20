a9030d8247c80abe32893a03070e60c5249af99e
Soso <solal.chatelain@gmail.com>
2024-12-20 14:47:08 +0100

    Update Joueur.cs

--- Joueur.cs
+++ Joueur.cs
@@ -11,8 +11,8 @@ namespace Projet_Boogle_Solal_JB
     {
         string nom;
         int score;
-        List<string> motsTrouves;
-        List<Lettre> lettres;
+        List<string> motsTrouves; /// liste des mots trouvés par le joueur
+        List<Lettre> lettres; /// liste des lettres composant les mots trouvés par le joueur
         public Joueur(string nom, List<Lettre> lettres)
         {
             this.nom = nom;
@@ -35,46 +35,26 @@ namespace Projet_Boogle_Solal_JB
             get { return motsTrouves; }
             set { motsTrouves = value; }
         }
-        public bool Contain(string mot)
+        public bool Contain(string mot) /// vérifie si le joueur a déjà trouvé le mot mis en paramètre
         {
-            bool rps = false;
-
+            bool retour = false;
             for (int i = 0; i < motsTrouves.Count; i++)
             {
-                if (mot == motsTrouves[i])
+                if (mot == motsTrouves[i]) /// on compare simplement le mot à chaque mot déjà trouvé
                 {
-                    rps = true;
+                    retour = true;
                 }
             }
-            return rps;
+            return retour;
         }
-        public bool Add_Mot(string mot)
+        public bool Add_Mot(string mot) /// ajoute le mot en paramètre à la liste des mots trouvés par le joueur
         {
             if (motsTrouves.Contains(mot))
             {
-                Console.WriteLine($"Vous avez déjà trouvé le mot {mot} !");
-                return false; // Le mot est déjà dans la liste
+                Console.WriteLine($"Vous avez déjà trouvé le mot {mot} !"); /// on n'éxecute pas si le mot a déjà été trouvé
+                return false;
             }
-            motsTrouves.Add(mot); // On ajoute le mot à la liste
-            int points = 0; // Calcul des points
-            mot = mot.ToUpper();
-            for (int i = 0; i < mot.Length; i++)
-            {
-                for (int j = 0; j < lettres.Count; j++)
-                {
-                    if (mot[i] == lettres[j].Caractere)
-                    {
-                        points += lettres[j].Points;
-                    }
-                }
-            }
-            score += points; // Ajout des points au score
-            Console.WriteLine($"+ {points}pts !");
-            return true; // Mot ajouté avec succès
-        }
-
-        public int CalculerPointsMot(string mot)
-        {
+            motsTrouves.Add(mot);
             int points = 0;
             mot = mot.ToUpper();
             for (int i = 0; i < mot.Length; i++)
@@ -87,9 +67,28 @@ namespace Projet_Boogle_Solal_JB
                     }
                 }
             }
+            score += points; /// on ajoute les points marqués par les lettres du mot au score du joueur
+            Console.WriteLine($"+ {points}pts !");
+            return true;
+        }
+
+        public int CalculerPointsMot(string mot) /// calcule les points marqués par un mot donné
+        {
+            int points = 0;
+            mot = mot.ToUpper();
+            for (int i = 0; i < mot.Length; i++)
+            {
+                for (int j = 0; j < lettres.Count; j++)
+                {
+                    if (mot[i] == lettres[j].Caractere)
+                    {
+                        points += lettres[j].Points; /// on ajoute simplement le score de chaque lettre à la valeur retournée
+                    }
+                }
+            }
             return points;
         }
-        public string toString()
+        public string toString() /// donne le score du joueur et le nombre de points qu'il a marqué
         {
             string description = "";
             description = $"{nom} a {score} points et a trouvé les mots suivants :\n";
